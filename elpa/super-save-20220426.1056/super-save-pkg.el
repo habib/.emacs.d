edcf2e71d0e3d2f1f02daad7fc2b1ab057e44cbf
Habib Pagarkar <habib@fullstory.com>
2022-05-02 02:25:45 -0500

    Weekly update

---
+++ elpa/super-save-20220426.1056/super-save-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from super-save.el  -*- no-byte-compile: t -*-
+(define-package "super-save" "20220426.1056" "Auto-save buffers, based on your activity." '((emacs "24.4")) :commit "71c26cbd47d993fff37e572523ea79c9c49f5caf" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("convenience") :url "https://github.com/bbatsov/super-save")
